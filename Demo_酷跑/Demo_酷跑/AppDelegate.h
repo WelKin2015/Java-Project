2f60f145a005ce33cb733127086b15eca3d07c54
WelKin2015 <xumengwei35@163.com>
2016-06-17 17:25:32 +0800

    Demo_酷跑
    Version-1

---
+++ Demo_酷跑/Demo_酷跑/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_酷跑
+//
+//  Created by WelKin Xu on 16/4/8.
+//  Copyright © 2016年 WelKin Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
